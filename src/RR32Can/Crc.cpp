6c4c255fe060c91e870f98f0bad4dddbfefad6ba
Damian Philipp <damian.philipp@gmx.net>
2019-02-24 22:32:28 +0100

    Added CRC functionality

---
+++ src/RR32Can/Crc.cpp
@@ -0,0 +1,17 @@
+#include "RR32Can/Crc.h"
+
+namespace RR32Can {
+
+void Crc::updateCrc(element_type input) {
+  accumulator ^= (input << 8);
+  for (int i = 0; i < 8; ++i) {
+    if ((accumulator & 0x8000) != 0) {
+      accumulator <<= 1;
+      accumulator ^= polynomial;
+    } else {
+      accumulator <<= 1;
+    }
+  }
+}
+
+} /* namespace RR32Can */
