bfa84558d13070387636774c308629156d77d057
Damian Philipp <damian.philipp@gmx.net>
2019-02-28 20:58:17 +0100

    Use VS Code Builtin clang

--- apply-clang.sh
+++ apply-clang.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+CLANG_BINARY="$HOME/.vscode/extensions/ms-vscode.cpptools-0.21.0/LLVM/bin/clang-format.exe"
+
 for filename in `find src -iname "*.cpp" -o -iname "*.h"`
 do
     echo "Formatting $filename"
-    "c:\Program Files\LLVM\bin\clang-format.exe" -i "$filename"
+    $CLANG_BINARY -i "$filename"
 done
 
 for filename in `find test -iname "*.cpp" -o -iname "*.h"`
 do
     echo "Formatting $filename"
-    "c:\Program Files\LLVM\bin\clang-format.exe" -i "$filename"
+    $CLANG_BINARY -i "$filename"
 done
